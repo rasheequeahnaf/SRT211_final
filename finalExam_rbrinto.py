d0c58ea6040d152f63f07ec5d090809a8be7ebd6
rbrinto <rbrinto@magi>
2024-04-13 04:14:14 -0400

    first commit with file

---
+++ finalExam_rbrinto.py
@@ -0,0 +1,11 @@
+# Define dictionaries for services, additional options, and service tiers with their prices
+dt_srvc = {
+            "Oil Change": 70.00,  "Tire Rotation": 60.00,  "Brake Inspection": 40.00, 
+            "Engine Tuning": 300.00, "Top-up Fluids": 20.00, "Allignment": 120.00,
+            "Winter Tire Change": 75.00, "Body damage repair": 3200.00, "Insurance Claim Service+": 300.00,
+            "Warranty Renewal": 335.00, "Vehicle Safety": 100.00, "": }
+
+dt_ext = {
+    "Car Wash": 20.00, "Interior Detailing": 30.00, "Engine bay Wash": 25.00, "Window Tint": 200.00,
+    "Paint Protection": 2859.00, "Vehicle vinyl wrapping": 7800.00, 
+}
