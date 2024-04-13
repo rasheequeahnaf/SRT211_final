df2955a87013820cc22d23705f47d67fb19a355c
rbrinto <rbrinto@magi>
2024-04-13 04:33:38 -0400

    Added more stuff

--- finalExam_rbrinto.py
+++ finalExam_rbrinto.py
@@ -12,20 +12,25 @@
 # Usage: ./<filename>.py
 
 
-
-
-
 # Defining dictionaries for services, additional options, and service tiers with their prices
 
-dt_srvc = { # dt = dictionary , srvc = service , ext = extras
+dt_srvc = { # dt = dictionary , srvc = service
     "Oil Change": 70.00,  "Tire Rotation": 60.00,  "Brake Inspection": 40.00, 
     "Engine Tuning": 300.00, "Top-up Fluids": 20.00, "Allignment": 120.00,
     "Winter Tire Change": 75.00, "Body damage repair": 3200.00, "Insurance Claim Service+": 300.00,
     "Warranty Renewal": 335.00, "Vehicle Safety": 100.00, "General inspection": 135.00 }
 
-dt_ext = {
+dt_ext = { # dt = dictionary , ext = extras
     "Car Wash": 20.00, "Interior Detailing": 30.00, "Engine bay Wash": 25.00, "Window Tint": 200.00,
     "Paint Protection": 2859.00, "Vehicle vinyl wrapping": 7800.00, "Paint Correction" : 400.00, 
-    "Engine Check": 340.00 }
+    "Engine Check": 340.00, "Tow Hitch installation": 1200.00, "Camping extras for Summer": 3795.00}
 
-dt_quality = {"Basic": 0.00, "Standard": 15.00, "Premium": 30.00 }
+dt_quality = {"Basic": 0.00, "Standard": 15.00, "Premium": 30.00 } # dt = dictionary 
+
+dt_vhcl_sz = {
+    "Sedan": 1.00, "Mid-Size Sedan": 1.13, "Hatchback": 1.20,
+    "Small SUV": 1.28, "Mid-Size SUV": 1.37, "Large SUV": 1.49,
+    "Truck": 2.36, "Tow Truck": 3.99, "Small Bus": 4.78, "Large Bus": 7.45}
+
+
+
