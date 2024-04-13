62fd921632ce1eea35becf1c1f3e4ef2c433e71c
rbrinto <rbrinto@magi>
2024-04-13 04:22:54 -0400

    Added author information

--- finalExam_rbrinto.py
+++ finalExam_rbrinto.py
@@ -1,11 +1,31 @@
-# Define dictionaries for services, additional options, and service tiers with their prices
-dt_srvc = {
-            "Oil Change": 70.00,  "Tire Rotation": 60.00,  "Brake Inspection": 40.00, 
-            "Engine Tuning": 300.00, "Top-up Fluids": 20.00, "Allignment": 120.00,
-            "Winter Tire Change": 75.00, "Body damage repair": 3200.00, "Insurance Claim Service+": 300.00,
-            "Warranty Renewal": 335.00, "Vehicle Safety": 100.00, "": }
+#!/usr/bin/env python3
+
+# Author: Rasheeque Ahnaf Brinto
+# Student ID: 154***600
+# Topic: Final Exam
+# Course: SRT211
+#
+# Description: Python program for a local vehicle maintenance shop that wants to digitize their service booking system
+# to improve service efficiency and customer experience. This program is menu-driven, allowing customers to select 
+# from a range of vehicle services, additional options, and service tiers, each affecting the total price of the booking.
+#
+# Usage: ./<filename>.py
+
+
+
+
+
+# Defining dictionaries for services, additional options, and service tiers with their prices
+
+dt_srvc = { # dt = dictionary , srvc = service , ext = extras
+    "Oil Change": 70.00,  "Tire Rotation": 60.00,  "Brake Inspection": 40.00, 
+    "Engine Tuning": 300.00, "Top-up Fluids": 20.00, "Allignment": 120.00,
+    "Winter Tire Change": 75.00, "Body damage repair": 3200.00, "Insurance Claim Service+": 300.00,
+    "Warranty Renewal": 335.00, "Vehicle Safety": 100.00, "General inspection": 135.00 }
 
 dt_ext = {
     "Car Wash": 20.00, "Interior Detailing": 30.00, "Engine bay Wash": 25.00, "Window Tint": 200.00,
-    "Paint Protection": 2859.00, "Vehicle vinyl wrapping": 7800.00, 
-}
+    "Paint Protection": 2859.00, "Vehicle vinyl wrapping": 7800.00, "Paint Correction" : 400.00, 
+    "Engine Check": 340.00 }
+
+dt_quality = {"Basic": 0.00, "Standard": 15.00, "Premium": 30.00 }
